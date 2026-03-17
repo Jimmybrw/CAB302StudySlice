e8fea77096ba22dedfa6cdbfb894fd42adc2e5d9
Jimmy <n12360619@qut.edu.au>
2026-03-17 15:05:25 +1000

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.cab302studyslice {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.cab302studyslice to javafx.fxml;
+    exports com.example.cab302studyslice;
+}
