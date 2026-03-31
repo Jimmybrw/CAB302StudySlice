90f1501bc7efa09fa2603b8708f96c100133a7f3
Josh <joshuas2904@gmail.com>
2026-03-31 12:59:09 +1000

    Login + register pages connected to MySQL Database

--- src/main/java/com/example/cab302studyslice/Core/Main.java
+++ src/main/java/com/example/cab302studyslice/Core/Main.java
@@ -9,12 +9,14 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         ViewManager.setStage(stage);
+
         try {
             Runtime.getRuntime().exec("powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -File tracker.ps1");
         } catch (Exception e) {
             System.err.println("Could not launch tracker.ps1. Ensure it is in the project root.");
         }
-        ViewManager.switchScene("home-view.fxml");
+
+        ViewManager.switchScene("login-view.fxml");
         stage.setTitle("StudySlice");
 
         stage.setOnCloseRequest(e -> {
