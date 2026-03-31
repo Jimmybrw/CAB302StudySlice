a22670fb95b8f51a803ededf0990f491837ae8dc
izy0854 <isabela.rangel@connect.qut.edu.au>
2026-03-31 17:00:24 +1000

    1. Fixed home page layout - empty as should be filled with data later on. 2. link to timer page + UI (can start timer and run it + set timer but this will be pushed to a separate page later)

--- src/main/java/com/example/cab302studyslice/Controller/StudyController.java
+++ src/main/java/com/example/cab302studyslice/Controller/StudyController.java
@@ -37,4 +37,5 @@ public class StudyController {
             isTracking = false;
         }
     }
+
 }
--- src/main/java/com/example/cab302studyslice/View/TimerView.java
+++ src/main/java/com/example/cab302studyslice/View/TimerView.java
@@ -1,3 +1,25 @@
-// Nothing here yet
+
 // this will have the mode you would like (start stop or set alarm)
 // alarm notification
+
+
+package com.example.cab302studyslice.View;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TimerView extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("timer-view.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Timer View");
+        stage.setScene(scene);
+
+
+    }
+}
