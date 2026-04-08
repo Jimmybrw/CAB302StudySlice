5872fe40b8d8bb4519f04f4c2cccea78b93cbd09
MiriamRay <miriam.r.bouraga@gmail.com>
2026-04-08 17:32:02 +1000

    drafted a basic outline of a session

--- src/main/java/com/example/cab302studyslice/Model/Activity.java
+++ src/main/java/com/example/cab302studyslice/Model/Activity.java
@@ -1,1 +1,35 @@
 // Nothing here yet
+package com.example.cab302studyslice.Model;
+
+public class Activity {
+    private String sessionId;
+    private String sessionLabel;
+    private String totalTime;
+    private String mostUsedApp;
+    private String focusScore;
+    private String summary;
+
+    public Activity(String sessionId, String sessionLabel, String mostUsedApp, String focusScore, String summary) {
+        this.sessionId = sessionId;
+        this.sessionLabel = sessionLabel;
+        this.mostUsedApp = mostUsedApp;
+        this.focusScore = focusScore;
+        this.summary = summary;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+    public String getSessionLabel() {
+        return sessionLabel;
+    }
+    public String getMostUsedApp() {
+        return mostUsedApp;
+    }
+    public String getFocusScore() {
+        return focusScore;
+    }
+    public String getSummary() {
+        return summary;
+    }
+}
