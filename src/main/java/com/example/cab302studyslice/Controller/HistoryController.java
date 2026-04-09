c84a346d72d2153512e2e63cc18b6f52410190ad
Dan1634 <n11633484@qut.edu.au>
2026-04-09 13:28:28 +1000

    Just testing for the history page

--- src/main/java/com/example/cab302studyslice/Controller/HistoryController.java
+++ src/main/java/com/example/cab302studyslice/Controller/HistoryController.java
@@ -1,1 +1,27 @@
-//nothing yet
+package com.example.cab302studyslice.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+public class HistoryController {
+
+    // Linked to  TextArea in history-view.fxml
+    @FXML
+    private TextArea historyTextArea;
+
+
+     //Replaces all current history text with new content.
+    public void setHistoryText(String historyText) {
+        if (historyTextArea != null) {
+            historyTextArea.setText(historyText);
+        }
+    }
+
+
+     //Adds one session entry to the history page.
+    public void appendSession(String sessionText) {
+        if (historyTextArea != null) {
+            historyTextArea.appendText("\n\n" + sessionText);
+        }
+    }
+}
