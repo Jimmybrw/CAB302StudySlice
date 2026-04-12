634d70ce6d1b994f34f508142a512f4b1ee8a677
MiriamRay <miriam.r.bouraga@gmail.com>
2026-04-12 19:46:59 +1000

    Working on the wrapped page

---
+++ src/main/java/com/example/cab302studyslice/Controller/WrapperController.java
@@ -0,0 +1,42 @@
+package com.example.cab302studyslice.Controller;
+
+import com.example.cab302studyslice.View.ViewManager;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class WrapperController {
+    @FXML
+    private Label sessionLabel;
+
+    @FXML
+    private Label summaryLabel;
+
+    @FXML
+    private Label totalTimeLabel;
+
+    @FXML
+    private Label mostUsedAppLabel;
+
+    @FXML
+    private Label focusScoreLabel;
+
+    @FXML
+    private Label insightLabel;
+
+    @FXML
+    public void initialize(){
+        // Placeholder data for now
+        sessionLabel.setText("Session Summary");
+        summaryLabel.setText("<brief summary of the session>");
+        totalTimeLabel.setText("<total time(00:00:00)>");
+        mostUsedAppLabel.setText("<Most Used App>");
+        focusScoreLabel.setText("<Focus Score>");
+        insightLabel.setText("Spent most of your time on this app?");
+    }
+
+    @FXML
+    private void onBackClick() {
+        ViewManager.switchScene("dashboard.fxml");
+    }
+
+}
